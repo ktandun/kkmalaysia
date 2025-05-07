2f6cf75a3fab42cddf75395f065d4cb734898d53
s-tandun <shinzietandun@gmail.com>
2025-05-07 16:45:00 +0700

    chore: create price view

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
 <script setup lang="ts">
 import MenuList from './components/MenuList.vue'
+import PriceCalculator from './components/PriceCalculator.vue';
 </script>
 
 <template>
-  <MenuList></MenuList>
+  <div class="app-main">
+    <MenuList></MenuList>
+    <PriceCalculator></PriceCalculator>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.app-main {
+  display: flex;
+}
+
+.app-main > :first-child {
+  flex-basis: 55%;
+}
+
+.app-main > :last-child {
+  flex-basis: 45%;
+}
+</style>
---
+++ src/components/PriceCalculator.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="price-box">
+    lorem ipsum
+  </div>
+</template>
+
+<style scoped>
+.price-box {
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.6); /* semi-transparent black */}
+</style>
